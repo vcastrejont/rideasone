29099565c2740e603915068d08690cb9afebd9f8
Victor Castrejon <vcastrejont@gmail.com>
2016-03-18 09:21:01 -0700

    db changes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': '^hr%ps}79TV2D&KJ',
     'database_local': 'mongodb://localhost/carpooling',
-    'database': 'mongodb://carpooling:welcome1@ds015869.mlab.com:15869/carpooling'
+    'database_labs': 'mongodb://carpooling:welcome1@ds015869.mlab.com:15869/carpooling'
     
 };
