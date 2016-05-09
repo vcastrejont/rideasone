6c6b56c509fe9f94453a44aa86c112cd51bdbdcb
Victor Castrejon <vcastrejont@gmail.com>
2016-05-09 15:19:35 -0700

    removed folders

--- carpoolingVan/www/js/passengers/services/passengersService.js
+++
@@ -1,6 +0,0 @@
-angular.module('carpoolingVan')
-
-.factory("Passengers", function($firebaseArray, firebaseRef) {
-  var itemsRef = new Firebase(firebaseRef + "users");
-  return $firebaseArray(itemsRef);
-})
