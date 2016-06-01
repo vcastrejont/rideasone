f5819099994b165674774f81f6dcda060e621eb3
ferzerkerx <ferzerkerx@gmal.com>
2016-06-01 07:13:46 +0000

    - added display route from car to destination

--- public/app/controllers/eventsShowCtrl.js
+++ public/app/controllers/eventsShowCtrl.js
@@ -1,6 +1,82 @@
 angular.module('carPoolingApp').controller('eventsShowCtrl', eventsShowCtrl);
 
+
 eventsShowCtrl.$inject = ['$scope', 'apiservice', '$state','$window'];
+
+
+//TODO Fer move this to a service maybe?
+//TODO Fer refactor
+function addDestinationMarkerToMap(map, myLatLng, content) {
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: ''
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: content
+    });
+    infowindow.open(map, marker);
+    (function (marker, content, leWindow, leMap) {
+        google.maps.event.addListener(marker, "click", function (e) {
+            leWindow.open(leMap, marker);
+        });
+    })(marker, content, infowindow, map);
+    return marker;
+}
+
+//TODO Fer refactor
+function addCarMarkerToMap(map, carPosition, content, directionsService,  directionsDisplay, eventDestinationMarker) {
+    var carMarker = new google.maps.Marker({
+        position: carPosition,
+        map: map,
+        title: ''
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: content
+    });
+
+    (function (marker, content, leWindow, leMap, eventMarker, directionsService, directionsDisplay) {
+        google.maps.event.addListener(marker, "click", function (e) {
+            //infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
+            leWindow.open(leMap, marker);
+
+            var bounds = new google.maps.LatLngBounds();
+            var end = eventMarker.position;
+            var start = marker.position;
+
+            bounds.extend(start);
+            bounds.extend(end);
+            leMap.fitBounds(bounds);
+
+            var request = {
+                origin: start,
+                destination: end,
+                travelMode: google.maps.TravelMode.DRIVING
+            };
+            directionsService.route(request, function (response, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(response);
+                    directionsDisplay.setMap(leMap);
+                } else {
+                    alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
+                }
+            });
+
+        });
+    })(carMarker, content, infowindow, map, eventDestinationMarker, directionsService, directionsDisplay);
+
+    return carMarker;
+}
+
+//TODO remove? move to backend?
+function stubData(cars) {
+    for (var i = 0; i < cars.length; i++) {
+        cars[i].location = [-86.7694625854492, 21.0904502868652];
+    }
+}
+
 
 function eventsShowCtrl ($scope, apiservice,  $state, $window) {
   $scope.id = $state.params.id;
@@ -38,28 +114,40 @@ function eventsShowCtrl ($scope, apiservice,  $state, $window) {
         console.error('Error: ' + response.data);
       });
     },
-    showMap:function(){
-      var myLatLng = {lat: this.event.location[1],lng: this.event.location[0]};
-      var options = {
-          center: new google.maps.LatLng(this.event.location[1], this.event.location[0]),
-          zoom: 17,
-          disableDefaultUI: false,
-          draggable: true
-      };
-      var mapCanvas = document.getElementById("map");
-      var map = new google.maps.Map(mapCanvas, options);
-      var infowindow = new google.maps.InfoWindow();
-      var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: ''
-      });
+      showMap: function () {
+          var event = this.event;
+          //TODO Fer remove
+          console.log("#######" + JSON.stringify(event));
+          var eventDestination = new google.maps.LatLng(event.location[1], event.location[0]);
+          var options = {
+              center: eventDestination,
+              zoom: 11,
+              disableDefaultUI: false,
+              draggable: true
+          };
 
-      var infowindow = new google.maps.InfoWindow({
-        content: this.event.place
-      });
-      infowindow.open(map, marker);
-    },
+          var directionsService = new google.maps.DirectionsService();
+
+          var mapCanvas = document.getElementById("map");
+          var map = new google.maps.Map(mapCanvas, options);
+
+          var directionsDisplay = new google.maps.DirectionsRenderer();
+          directionsDisplay.setMap(map);
+
+
+          var eventDestinationMarker = addDestinationMarkerToMap(map, eventDestination, event.place);
+
+          var cars = event.cars;
+
+          //TODO Fer remove
+          stubData(cars);
+
+          var car;
+          for (var i = 0; i < cars.length; i++) {
+              car = cars[i];
+              addCarMarkerToMap(map, new google.maps.LatLng(car.location[1], car.location[0]) , car.driver_name, directionsService, directionsDisplay, eventDestinationMarker);
+          }
+      },
     clearOptions:function(){
       this.seats = "";
       this.driver = "";
