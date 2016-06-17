35a5c40fec846ba25df0a8c45b3d7b10497b38ea
Javier Rosas <javier.rosasb@gmail.com>
2016-06-16 18:53:10 -0700

    add method to requestJoining ride

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,23 @@
 var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+var Schema = mongoose.Schema;
 
-var userSchema = new Schema({	name: String,  provider: String,  provider_id: {type: String, unique: true},  photo: String,	email: String,  createdAt: {type: Date, default: Date.now}});
+var UserSchema = new Schema({
+  name: String,
+  provider: String,
+  provider_id: {type: String, unique: true},
+  photo: String,
+  email: String,
+  createdAt: {type: Date, default: Date.now}
+});
 
-module.exports = mongoose.model('user', userSchema);
+UserSchema.methods.requestJoiningRide = function (rideId) {
+  var RideRequest = require('./rideRequest');
+  var request = new RideRequest({
+    ride: rideId,
+    passenger: this
+  });
+  // TODO: Create driver notification
+  return request.save();
+};
+
+module.exports = mongoose.model('user', UserSchema);
