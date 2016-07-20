3dc1b26a794c90b651f3686f427e627a2f661612
lexas <dislexas@gmail.com>
2016-07-20 11:36:15 -0600

    test event removal

---
+++ tests/events/removeEvent.js
@@ -0,0 +1,59 @@
+var supertest = require('supertest-as-promised');
+var assert = require('chai').assert;
+var port = process.env.PORT|| 3000;
+var req = supertest('http://localhost:'+ port +'/api');
+var User = require('../../models/User');
+var Place = require('../../models/Place');
+var Event = require('../../models/Event');
+var token, testUser, testEvent;
+
+describe('Event removal', function(){
+  before(() => {
+    return new User({
+      name:'test', 
+      email:'test@test.com', 
+      provider_id: Date.now()
+    })
+    .save()
+    .then(user => { 
+      testUser = user;
+      return supertest('http://localhost:'+ port)
+        .post('/auth/fakeAuthForTesting')
+        .send({userId: testUser._id});
+    })
+    .then(res => {
+      token = 'JWT '+ res.body.token;
+    })
+    .then(() => {
+      return new Event({
+        name: "feria del pollo Z", 
+        description: "una feria de pollos", 
+        address: "pollolandia", 
+        location: "pollornia", 
+        organizer: testUser._id, 
+        datetime: new Date("2017-05-05T02:20:10Z"), 
+        tags: ['feria', 'pollo']
+      })
+      .save();
+    })
+    .then(event => {
+      testEvent = event;
+    });
+  });
+
+  after(() => {
+    return testUser.remove()
+    .then(() => testEvent.remove());
+  });
+
+  it('removes an event organized by requesting user', () => {
+      return req.delete('/events/'+ testEvent._id)
+        .set('Authorization', token)
+        .expect(200)
+        .then(res => {
+          var event = res.body;
+          assert.equal(event._id, testEvent._id);
+        });   
+  });
+
+});
