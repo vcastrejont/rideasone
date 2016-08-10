41c70d307635b645830a9af61ed561b906b10a94
Victor Castrejon <vcastrejont@gmail.com>
2016-08-10 12:43:48 -0700

    grunt watch

--- public/rideasone.js
+++ public/rideasone.js
@@ -544,40 +544,8 @@ angular.module('carPoolingApp').controller('profileCtrl', profileCtrl);
 profileCtrl.$inject = ['$scope','apiservice','mapFactory' ];
 
 function profileCtrl ($scope, apiservice, mapFactory) {
-
-
-  
-
-}
-
-angular.module('carPoolingApp').controller('setDefaultCtrl', setDefaultCtrl);
-
-setDefaultCtrl.$inject = ['$scope', '$window', 'apiservice'];
-
-function homeCtrl ($scope, $window, apiservice) {
-  var options = {
-      center: new google.maps.LatLng(29.0821369,-110.9572747),
-      zoom: 13,
-      disableDefaultUI: true,
-      draggable: true
-  };
-  var mapCanvas = document.getElementById("map");
-  var map = new google.maps.Map(mapCanvas, options);
-
-  apiservice.getEvents()
-    .success(function(data) {
-        $scope.nextEvents=data;
-    })
-    .error(function(data) {
-        console.error('Error: ' + data);
-    });
-  apiservice.getPastEvents()
-    .success(function(data) {
-        $scope.pastEvents=data;
-    })
-    .error(function(data) {
-        console.error('Error: ' + data);
-    });
+  $scope.api = mapFactory.getApi();
+  $scope.api.defaultLocation();
 }
 
 angular.module('carPoolingApp').controller('settingsCtrl', settingsCtrl);
